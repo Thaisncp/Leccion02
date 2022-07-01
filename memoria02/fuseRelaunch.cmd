09706b0e68c214f4323d7ee679617a0cfd8cecf8
Thais Cartuche <93340721+Thaisncp@users.noreply.github.com>
2022-07-01 12:53:09 -0500

    Add files via upload

---
+++ memoria02/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/Users/ASUS/Desktop/Xiling/EjerciciosU2/memoria02/memoria2_isim_beh.exe" -prj "C:/Users/ASUS/Desktop/Xiling/EjerciciosU2/memoria02/memoria2_beh.prj" "work.memoria2" 
---
+++ memoria02/memoria2.vhd
@@ -0,0 +1,42 @@
+  LIBRARY ieee;
+  USE ieee.std_logic_1164.ALL;
+  USE ieee.numeric_std.ALL;
+
+  ENTITY memoria2 IS
+	port(
+		clk : in std_logic;
+		en : in std_logic;
+		we : in std_logic;
+		rst : in std_logic;
+		addr : in std_logic_vector(9 downto 0);
+		di : in std_logic_vector(15 downto 0);
+		do : out std_logic_vector(15 downto 0)
+		);
+  END memoria2;
+
+  ARCHITECTURE behavior OF memoria2 IS 
+
+  type ram_type is array (1023 downto 0) of std_logic_vector (15 downto 0);
+  
+  SIGNAL ram : ram_type;
+   begin
+process(clk)       
+
+  BEGIN
+
+		if clk'event and clk = '1' then 
+		if en = '1' then
+		if we = '1' then
+		ram(conv_integer(addr)) <= di;
+		end if;
+		
+		if rst = '1' then 
+		do <= (others => '0');
+		else
+		do <= ram(conv_integer(addr));
+		end if;
+		end if;
+		end if;
+		end process; 
+
+  END;
